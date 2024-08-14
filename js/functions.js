fd44e1c61e38d7607cc3c87a0edf2cf3e2c15437
Игорь <igoryha96@gmail.com>
2024-08-15 01:14:24 +0300

    Добавляет функцию для выполнения задания

--- js/functions.js
+++ js/functions.js
@@ -36,19 +36,23 @@ const getResultTime = (timeStart, timeEnd, timeStartMeeting, timeMeeting) => {
   const timeEndToMinutes = getMinutesOfHour(timeEnd);
   const timeStartMeetingToMinutes = getMinutesOfHour(timeStartMeeting);
 
-  const timeWork = (timeStartMeetingToMinutes - timeEndToMinutes);
+  const timeWork = (timeEndToMinutes - timeStartMeetingToMinutes);
 
   if (timeStartMeetingToMinutes < timeStartToMinutes) {
-    false
-  };
+    return false;
+  }
 
   if (timeWork >= timeMeeting) {
-    true
+    return true;
   }
-}
+  else {
+    return false;
+  }
+};
 
-getResultTime('08:00', '17:30', '14:00', 90); // true
+
 getResultTime('8:0', '10:0', '8:0', 120);// true
+getResultTime('08:00', '17:30', '14:00', 90); // true
 getResultTime('08:00', '14:30', '14:00', 90); // false
 getResultTime('14:00', '17:30', '08:0', 90);// false
 getResultTime('8:00', '17:30', '08:00', 900); // false
